460181c4cd3d960338fe8eca5ba44e41698d1964
andredisa <andreadisanti22@gmail.com>
2024-05-20 20:41:33 +0200

    gestione per la login

--- progetto/ajax/login_process.php
+++ progetto/ajax/login_process.php
@@ -1,40 +1,39 @@
 <?php
-require_once("../classi/gestioneDB.php");
+    require_once("../classi/gestioneDB.php");
 
-if(!isset($_SESSION))
-{
-    session_start();
-}
+    if(!isset($_SESSION)) {
+        session_start();
+    }
 
-// Inizializza la classe gestioneDatabase
-$gest = new gestioneDB();
-$gest->conn();
+    // Inizializza la classe gestioneDatabase
+    $gest = new gestioneDB();
+    $gest->conn();
 
-// Inizializza la risposta JSON
-$response = ["status" => false];
+    // Inizializza la risposta JSON
+    $response = ["status" => false];
 
-if(isset($_GET["email"]) && isset($_GET["password"])) {
-    $email = $_GET['email'];
-    $password = $_GET['password'];
+    if(isset($_POST["email"]) && isset($_POST["password"])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-    // Controllo se è un admin
-    $is_admin = $gest->authenticateAdmin($email, $password);
-    if($is_admin === true) {
-        $_SESSION["admin"] = true;
-        $response["status"] = "admin";
-    } elseif($is_admin === false) {
-        // Se non è admin, controllo se è un cliente
-        $is_cliente = $gest->authenticateUser($email, $password);
-        if($is_cliente === true) {
-            $_SESSION["admin"] = false;
-            $response["status"] = "cliente";
+        // Controllo se è un admin
+        $is_admin = $gest->authenticateAdmin($email, $password);
+        if($is_admin === true) {
+            $_SESSION["admin"] = true;
+            $response["status"] = "admin";
+        } elseif($is_admin === false) {
+            // Se non è admin, controllo se è un cliente
+            $is_cliente = $gest->authenticateUser($email, $password);
+            if($is_cliente === true) {
+                $_SESSION["admin"] = false;
+                $response["status"] = "cliente";
+            }
         }
     }
-}
 
-// Chiudi la connessione al database
-$gest->connection->close();
+    // Chiudi la connessione al database
+    $gest->connection->close();
 
-// Restituisci la risposta JSON
-echo json_encode($response);
+    // Restituisci la risposta JSON
+    echo json_encode($response);
 ?>
