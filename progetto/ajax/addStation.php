2eb9234e37a5f826e885888cb6ae78a1cfac08c7
andredisa <andreadisanti22@gmail.com>
2024-05-21 20:12:17 +0200

    modifica metodo

--- progetto/ajax/addStation.php
+++ progetto/ajax/addStation.php
@@ -2,7 +2,7 @@
     require_once("../classi/gestioneDB.php");
     $response = ["status" => false, "message" => "Errore durante l'aggiunta della stazione."];
 
-    if (isset($_POST['codice'], $_POST['numero_slot'], $_POST['via'], $_POST['città'], $_POST['provincia'], $_POST['regione'], $_POST['CAP'])) {
+    if (isset($_POST['codice'], $_POST['numero_slot'], $_POST['via'], $_POST['città'], $_POST['provincia'], $_POST['regione'], $_POST['CAP'], $_POST['latitudine'], $_POST['longitudine'])) {
         $gest = new gestioneDB();
         $gest->conn();
 
@@ -13,8 +13,10 @@
         $provincia = $_POST['provincia'];
         $regione = $_POST['regione'];
         $CAP = $_POST['CAP'];
+        $latitudine = $_POST['latitudine'];
+        $longitudine = $_POST['longitudine'];
 
-        if ($gest->addStation($codice, $numero_slot, $via, $città, $provincia, $regione, $CAP)) {
+        if ($gest->addStation($codice, $numero_slot, $via, $città, $provincia, $regione, $CAP, $latitudine, $longitudine)) {
             $response["status"] = true;
             $response["message"] = "Stazione aggiunta con successo.";
         }
