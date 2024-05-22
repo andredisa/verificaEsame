e0f5f567f614253794a4914ded183d6cac424c7f
andredisa <andreadisanti22@gmail.com>
2024-05-22 16:22:32 +0200

    modifica db

--- db/db_esame.sql
+++ db/db_esame.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 20, 2024 alle 19:52
+-- Creato il: Mag 22, 2024 alle 14:59
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -40,7 +40,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`ID`, `email`, `password`, `nome`, `cognome`) VALUES
-(1, 'andreadisanti22gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'andrea', 'disanti');
+(1, 'andreadisanti22@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'andrea', 'disanti');
 
 -- --------------------------------------------------------
 
@@ -53,9 +53,9 @@ CREATE TABLE `bicicletta` (
   `codice` varchar(16) NOT NULL,
   `distanza_percorsa` float DEFAULT NULL,
   `posizione_attuale` varchar(64) NOT NULL,
-  `GPS` varchar(16) NOT NULL,
+  `GPS` varchar(50) NOT NULL,
   `attiva` tinyint(1) NOT NULL,
-  `RFID` varchar(16) NOT NULL
+  `RFID` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -78,6 +78,13 @@ CREATE TABLE `cliente` (
   `regione` varchar(32) NOT NULL,
   `CAP` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dump dei dati per la tabella `cliente`
+--
+
+INSERT INTO `cliente` (`ID`, `email`, `password`, `nome`, `cognome`, `numero_tessera`, `numero_carta_credito`, `via`, `città`, `provincia`, `regione`, `CAP`) VALUES
+(1, 'matteorossi@gmail.com', 'aa9a0040f7c00a74d0362b8c4a214b6e', 'matteo', 'rossi', 12345, 2147483647, 'via Prealpi 47', 'Mariano Comense', 'CO', 'Lombardia', 22066);
 
 -- --------------------------------------------------------
 
@@ -110,8 +117,17 @@ CREATE TABLE `stazione` (
   `città` varchar(32) NOT NULL,
   `provincia` varchar(32) NOT NULL,
   `regione` varchar(32) NOT NULL,
-  `CAP` int(11) NOT NULL
+  `CAP` int(11) NOT NULL,
+  `latitudine` float NOT NULL,
+  `longitudine` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dump dei dati per la tabella `stazione`
+--
+
+INSERT INTO `stazione` (`ID`, `codice`, `numero_slot`, `via`, `città`, `provincia`, `regione`, `CAP`, `latitudine`, `longitudine`) VALUES
+(1, '1234', 30, 'via Prealpi 47', 'Mariano Comense', 'CO', 'Lombardia', 22066, 45.6993, 9.1883);
 
 --
 -- Indici per le tabelle scaricate
@@ -171,7 +187,7 @@ ALTER TABLE `bicicletta`
 -- AUTO_INCREMENT per la tabella `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT per la tabella `operazione`
@@ -183,7 +199,7 @@ ALTER TABLE `operazione`
 -- AUTO_INCREMENT per la tabella `stazione`
 --
 ALTER TABLE `stazione`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Limiti per le tabelle scaricate
